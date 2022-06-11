6fe90efab09cd29a9f08ec0cd70fb229d9a6822b
Mihails Iljicevs <mihails.iljicevs@trialine.lv>
2022-06-11 20:08:27 +0300

    change settings

--- home/settings.py
+++ home/settings.py
@@ -62,10 +62,7 @@ TEMPLATES = [
     },
 ]
 
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'assets', 'dist'),
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 WSGI_APPLICATION = 'home.wsgi.application'
 
@@ -116,7 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'stat')
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'assets', 'dist'),
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
