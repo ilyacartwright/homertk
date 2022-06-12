5926a34f0bebab13e1394fce7813cb79c320e8e9
Mihails Iljicevs <mihails.iljicevs@trialine.lv>
2022-06-12 03:51:56 +0300

    test

--- index/urls.py
+++ index/urls.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 from django.urls import path
-from .views import render_index_page
+from .views import render_index_page, Testik
 
 app_name = 'index'
 
 
 urlpatterns = [
-    path('', render_index_page, name="home"),
+    path('', Testik.as_view(), name="home"),
 
 ]
--- index/views.py
+++ index/views.py
@@ -4,6 +4,9 @@ from django.http import Http404
 from django.shortcuts import render
 from .models import CompanyGoal, Department
 
+class Testik(TemplateView):
+    template_name = 'page/home.html'
+
 def render_index_page(request):
 
     goal = CompanyGoal.objects.all()
